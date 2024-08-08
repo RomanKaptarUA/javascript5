9dd0ac2a1575f22dc58545087df7c406f803c8f4
RomanKaptarUA <romankaptar82@gmail.com>
2024-08-08 19:53:53 +0300

    ю

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-// 1
-if ()
+let onechange = showChoice;
+switch(onechange){
+    case 1:
+        prompt('write');
+        if (prompt = coffee);
+       console.log('кава');
+        case 2:
+            drink = tea;
+            console.log('ЧАЙ');
+            case 3:
+
+                drink = juice;
+                console.log('сік');
+                
+                default:
+                    console.log('Нічого не вибрано');
+}
